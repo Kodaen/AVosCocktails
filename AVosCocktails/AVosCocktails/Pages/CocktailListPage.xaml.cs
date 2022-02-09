b12e85ee931f2401e986859b4dc472623fd1737b
Kodaen <tristanbadana@gmail.com>
2022-02-09 16:22:09 +0100

    marche stp

---
+++ AVosCocktails/AVosCocktails/Pages/CocktailListPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CocktailDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace AVosCocktails.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class CocktailListPage : ContentPage
+    {
+        public CocktailListPage()
+        {
+            InitializeComponent();
+            test();
+        }
+
+        public void test()
+        {
+            foreach (var i in CocktailAPI.Search("Vodka"))
+            {
+                Console.Write(i.idDrink + " ;");
+                Console.WriteLine(i.strDrink);
+            }
+        }
+    }
+}
